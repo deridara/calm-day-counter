de91f22044722780c652de3ebf8c2a0ba761342a
dara <teriaderide@gmail.com>
2018-08-18 00:03:35 +0300

    немного позиционирования

--- script.js
+++ script.js
@@ -11,8 +11,13 @@ $(document).ready(() => {
 
     $('.count').text(+incCount);
 
+    $('.count-text').text(countString(incCount));
+
     $('#new-inc-btn').on('click', () => {
+        
         setZero();
+
+        $('.count-text').text(countString(incCount));
         const text = $("#new-inc-text").val();
         const d = new Date;
         const z = d.getMonth() < 9 ? '0' : ''
@@ -27,4 +32,16 @@ const increaseByOne = () => {
     incCount++;
     $('.count').text(+incCount);
     console.log(typeof incCount, typeof + incCount);
+}
+
+const wordForNum = (str1, str2, str3) => (num) => {
+    if (num > 4 && num < 21) return str3;
+    if (num % 10 === 1) return str1;
+    if (num % 10 > 1 && num % 10 < 5) return str2;
+    return str3;
+}
+
+const countString = (num) => {
+    const days = wordForNum('День', 'Дня', 'Дней')(num);
+    return `${days} без происшествий`;
 }
